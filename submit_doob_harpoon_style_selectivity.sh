26e58b4250956e364f9869c9f9c83d2876514a80
Aditya Agrawal <ayane@dhcp-86-211.eduroam.aalto.fi>
2026-08-28 19:55:07 +0300

    Isolate tight Doob curriculum samples

--- submit_doob_harpoon_style_selectivity.sh
+++ submit_doob_harpoon_style_selectivity.sh
@@ -7,8 +7,8 @@ cd /scratch/work/agrawaa4/TabDiff
 DATANAME="${DATANAME:-shoppers}"
 MODEL_NAME="${MODEL_NAME:-ft_periodic_seed0}"
 QUERY_DIR="${QUERY_DIR:-data90/${DATANAME}/queries_full}"
-DOOB_GUIDE_DIR="${DOOB_GUIDE_DIR:-tabdiff/ckpt/${DATANAME}/${MODEL_NAME}/doob_query_curriculum_d48_l2_12000}"
-DOOB_LABEL="${DOOB_LABEL:-doob_curriculum}"
+DOOB_GUIDE_DIR="${DOOB_GUIDE_DIR:-tabdiff/ckpt/${DATANAME}/${MODEL_NAME}/doob_query_tight_curriculum_d48_l2_18000}"
+DOOB_LABEL="${DOOB_LABEL:-doob_tight_curriculum_s50}"
 HARPOON_STYLE_LABEL="${HARPOON_STYLE_LABEL:-harpoon_style_tabdiff_eta02_s50}"
 HARPOON_STYLE_ETA="${HARPOON_STYLE_ETA:-0.2}"
 NUM_TIMESTEPS="${NUM_TIMESTEPS:-50}"
@@ -16,10 +16,11 @@ NUM_SAMPLES="${NUM_SAMPLES:-1000}"
 MAX_CONCURRENT="${MAX_CONCURRENT:-8}"
 SEED_BASE="${SEED_BASE:-10000}"
 
-# These can point at any existing compatible sample directories. The default
-# Doob path is the directory produced by the earlier full query-suite run.
+# These can point at any existing compatible sample directories. Keep the tight
+# curriculum outputs separate so older curriculum samples cannot be reused by
+# mistake merely because they have matching query filenames.
 SUITE_SAMPLE_ROOT="${SUITE_SAMPLE_ROOT:-conditional_samples/${DATANAME}/doob_vs_harpoon_style_s50}"
-DOOB_SAMPLE_DIR="${DOOB_SAMPLE_DIR:-conditional_samples/${DATANAME}/query_suite_comparison/${DOOB_LABEL}}"
+DOOB_SAMPLE_DIR="${DOOB_SAMPLE_DIR:-${SUITE_SAMPLE_ROOT}/${DOOB_LABEL}}"
 HARPOON_STYLE_SAMPLE_DIR="${HARPOON_STYLE_SAMPLE_DIR:-${SUITE_SAMPLE_ROOT}/${HARPOON_STYLE_LABEL}}"
 OUTPUT_DIR="${OUTPUT_DIR:-evaluations/${DATANAME}/doob_vs_harpoon_style_s50}"
 
